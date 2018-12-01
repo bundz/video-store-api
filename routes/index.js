2503fe4f6bc4b80d4f0a9d9e223ba469f115a643
Vitor Alano <vitor.alano@nodo.cc>
2018-12-01 01:16:01 -0200

    Fix on video routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require(`express`).Router();
 
 router.use(`/user`, require(`./user`));
 router.use(`/auth`, require(`./auth`));
-// router.use(`/video`, require(`./role`));
+router.use(`/video`, require(`./role`));
 
 module.exports = router;
